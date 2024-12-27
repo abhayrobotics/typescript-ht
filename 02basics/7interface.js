49265f243cc70b2bcf909e5b55ec31ce15d4366a
Abhay kumar <abhayrobotics1908@gmail.com>
2024-12-27 13:43:12 +0530

    new TS config setup

--- 02basics/7interface.js
+++ 02basics/7interface.js
@@ -1,7 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 var AB = {
     id: 23,
     name: 'Abhay',
     email: "a@x.com",
+    githubLogin: "Abhayrobotics",
     getStart: function () {
         return "Hell";
     },
@@ -11,4 +14,26 @@ var AB = {
     getCode: function (code) {
         return 10;
     },
+};
+var AB2 = {
+    id: 23,
+    admin: "Yes",
+    name: 'Abhay',
+    email: "a@x.com",
+    githubLogin: "Abhayrobotics",
+    getStart: function () {
+        return "Hell";
+    },
+    getStart2: function () {
+        return "Hell";
+    },
+    getCode: function (code) {
+        return 10;
+    },
+};
+var Raj = {
+    getFood: function () {
+        return "hell";
+    },
+    name: "ABhay"
 };
