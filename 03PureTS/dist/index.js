af75a9053ab628df93b0c19a996ab94d2f2a31dc
Abhay kumar <abhayrobotics1908@gmail.com>
2024-12-29 11:16:06 +0530

    classes

--- 03PureTS/dist/index.js
+++ 03PureTS/dist/index.js
@@ -1,2 +1,10 @@
 "use strict";
 console.log("typescript is wor king");
+// classes\
+class User {
+    constructor(email, name) {
+        this.email = email;
+        this.name = name;
+    }
+}
+let abhay = new User("a@a", "Abhay");
--- 03PureTS/src/index.ts
+++ 03PureTS/src/index.ts
@@ -1,1 +1,15 @@
 console.log("typescript is wor king");
+
+// classes\
+
+class User {
+
+    email:string;
+    name:string
+    constructor(email:string,name:string){
+        this.email = email;
+        this.name =name;
+    }
+}
+
+let abhay = new User ("a@a","Abhay")
