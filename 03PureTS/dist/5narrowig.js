158e45e938618d93fef413022e789a3bee57c184
Abhay kumar <abhayrobotics1908@gmail.com>
2024-12-31 01:28:08 +0530

    cmplet typescipt

--- 03PureTS/dist/5narrowig.js
+++ 03PureTS/dist/5narrowig.js
@@ -34,3 +34,35 @@ function checkAdminAccount(item) {
         return item.isAdmin;
     }
 }
+// ! instanceof keyword
+function logValue(x) {
+    if (x instanceof Date) {
+        console.log(x.toUTCString());
+    }
+    else {
+        console.log(x.toUpperCase());
+    }
+}
+function isFish(pet) {
+    return pet.swim !== undefined;
+}
+function getTrueShape(shape) {
+    if (shape.kind === "circle") {
+        return Math.PI * shape.radius ** 2;
+    }
+    //return shape.side * shape.side
+}
+// exhaustice checking with never
+function getArea(shape) {
+    switch (shape.kind) {
+        case "circle":
+            return Math.PI * shape.radius ** 2;
+        case "square":
+            return shape.side * shape.side;
+        case "rectangle":
+            return shape.length * shape.width;
+        default:
+            const _defaultforshape = shape;
+            return _defaultforshape;
+    }
+}
--- 03PureTS/src/5narrowig.ts
+++ 03PureTS/src/5narrowig.ts
@@ -1,52 +1,117 @@
 // narrowing
 // it means that when we have multiple data type , every data type must be handled, by narrowing anad chekcing each
 
-function detect (val:string | number ){
-    if(typeof val ==="string"){
-        val.toLowerCase();
-        return;
-    }
+function detect(val: string | number) {
+  if (typeof val === "string") {
+    val.toLowerCase();
+    return;
+  }
 
-    return val+3;
+  return val + 3;
 }
 
 // here we used typeof property to narrow down each data type and check.
 
-function f2(val:string | null){
-    // if not null , ie string
-    if(!val){
-        return val;
-    }
-    val.toLowerCase();
+function f2(val: string | null) {
+  // if not null , ie string
+  if (!val) {
+    return val;
+  }
+  val.toLowerCase();
 }
-
 
 // documentation example
 function printAll(strs: string | string[] | null) {
-    if (strs) {
-      if (typeof strs === "object") {
-        for (const s of strs) {
-          console.log(s);
-        }
-      } else if (typeof strs === "string") {
-        console.log(strs);
+  if (strs) {
+    if (typeof strs === "object") {
+      for (const s of strs) {
+        console.log(s);
       }
+    } else if (typeof strs === "string") {
+      console.log(strs);
     }
   }
-  
+}
+
 //! in keyword use  in narrowing
 
-interface User{
-    name:string,
-    email:string
+interface User {
+  name: string;
+  email: string;
 }
 
-interface Admin extends User{
-    isAdmin :boolean
+interface Admin extends User {
+  isAdmin: boolean;
 }
 
-function checkAdminAccount(item: User |Admin){
-    if( "isAdmin" in item){
-        return item.isAdmin;
+function checkAdminAccount(item: User | Admin) {
+  if ("isAdmin" in item) {
+    return item.isAdmin;
+  }
+}
+
+// ! instanceof keyword
+function logValue(x: Date | string) {
+  if (x instanceof Date) {
+    console.log(x.toUTCString());
+  } else {
+    console.log(x.toUpperCase());
+  }
+}
+
+
+// !  is and as keyword , type precates watch documentation
+type Fish = { swim: () => void };
+type Bird = { Fly: () => void };
+
+function isFish(pet: Fish | Bird): pet is Fish {
+  return (pet as Fish).swim !== undefined;
+}
+
+
+// ! Discriminated unions
+
+
+interface Circle {
+    kind: "circle",
+    radius: number
+}
+
+interface Square {
+    kind: "square"
+    side: number
+}
+
+interface Rectangle {
+    kind: "rectangle",
+    length: number,
+    width: number
+}
+
+type Shape = Circle | Square | Rectangle
+
+function getTrueShape(shape: Shape){
+    if (shape.kind === "circle") {
+        return Math.PI * shape.radius ** 2
+    }
+    //return shape.side * shape.side
+}
+
+
+// exhaustice checking with never
+
+function getArea(shape: Shape){
+    switch(shape.kind){
+        case "circle":
+            return Math.PI * shape.radius ** 2
+
+        case "square":
+            return shape.side * shape.side
+        case "rectangle":
+            return shape.length * shape.width
+
+        default:
+            const _defaultforshape: never = shape
+            return _defaultforshape
     }
 }
